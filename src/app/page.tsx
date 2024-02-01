8b326c53c9b9b3f60cd666cc01a9de76c6319807
luis-henrique-carvalho <luisdev9225@gmail.com>
2024-01-31 21:22:11 -0300

    refactor: page component to use separate sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,20 @@
 import React from "react";
 // Prismic
 import { createClient } from "@/prismicio";
-import { PrismicRichText } from "@prismicio/react";
-import Button from "@/components/Button";
-import Image from "next/image";
-import ImageComponent from "@/components/ImageComponent";
+// Components
+import TestimonialSection from "./components/TestimonialSection";
+import HeaderSection from "./components/HeaderSection";
+import AboutSection from "./components/AboutSection";
 
 const page = async () => {
   const client = createClient();
   const home = await client.getSingle("home");
 
-  console.log(home.data.image_1);
-
   return (
     <div>
-      <PrismicRichText field={home.data.text_1} />
-
-      <div className="flex fle-row justify-between">
-        <Button name="Entre em contato" styles="primary" href="/" />
-        <Button name="Saiba mais" styles="secondary" href="/" />
-        <ImageComponent
-          src={home.data.image_1.url!}
-          alt={home.data.image_1.alt!}
-          width={home.data.image_1.dimensions!.width}
-          height={home.data.image_1.dimensions!.height}
-        />
-      </div>
+      <HeaderSection homeData={home} />
+      <AboutSection homeData={home} />
+      <TestimonialSection homeData={home} />
     </div>
   );
 };
