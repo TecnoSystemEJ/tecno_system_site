facba45caacc58e73042c3eb506507890f292658
Erivan <erivantheone@gmail.com>
2024-02-02 12:05:26 -0300

    :sparkles: feat: Add about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-// Flow
 import React from "react";
-// Prismic
 import { createClient } from "@/prismicio";
-// Components
 import TestimonialSection from "./components/TestimonialSection";
 import HeaderSection from "./components/HeaderSection";
 import AboutSection from "./components/AboutSection";
