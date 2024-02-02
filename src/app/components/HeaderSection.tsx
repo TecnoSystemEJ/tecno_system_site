a05011c5cdf4061e104223b05e75b51b644fa16a
Wallace Sander <90098336+1Sander@users.noreply.github.com>
2024-02-02 16:12:42 -0300

    feat: Add header section

--- src/app/components/HeaderSection.tsx
+++ src/app/components/HeaderSection.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { AllDocumentTypes } from "../../../prismicio-types";
+import TextCard from "@/components/TextCard";
+import NavBar from "@/components/NavBar";
+import Image from "next/image";
 
 type Props = {
   homeData: AllDocumentTypes;
@@ -8,7 +11,86 @@ type Props = {
 const HeaderSection = (props: Props) => {
   const text1 = props.homeData.data;
 
-  return <div></div>;
+  return (
+    <div className="bg-gray-alt p-4">
+      <NavBar></NavBar>
+
+      <div className="flex flex-col text-xl text-red-500 text-righteous font-primary">TECNOSYSTEM</div>
+
+      <div className="font-secondary text-black text-2xl font-medium mb-8">
+        TRANSFORMANDO IDEIAS EM{" "}
+        <div className="font-primary text-red-500 text-2xl font-medium inline">
+          TECNOLOGIA
+        </div>
+      </div>
+
+      <div className="font-secondary text-black text-xs font-medium flex items-center">
+        <Image
+          src={"/homeicons/glyph.svg"}
+          alt="Logo"
+          height={15}
+          width={15}
+          className="mr-1"
+        />
+        DO CONCEITO À REALIDADE DIGITAL
+      </div>
+
+
+      <div className="font-secondary text-black text-xs font-medium flex items-center">
+        <Image
+          src={"/homeicons/glyph.svg"}
+          alt="Logo"
+          height={15}
+          width={15}
+          className="mr-1"
+        />SOLUÇÃO DE SOFTWARE PERSONALIZADAS</div>
+
+            <div className="font-secondary text-black text-xs font-medium flex items-center">
+        <Image
+          src={"/homeicons/glyph.svg"}
+          alt="Logo"
+          height={15}
+          width={15}
+          className="mr-1"
+        />INOVCAÇÃO NA PONTA DOS SEUS DEDOS</div>
+
+            <div className="font-secondary text-black text-xs font-medium flex items-center">
+        <Image
+          src={"/homeicons/glyph.svg"}
+          alt="Logo"
+          height={15}
+          width={15}
+          className="mr-1"
+        />SUA VISÃO, NOSSA EXPERIÊNCIA</div>
+
+      <div className="flex justify-center space-x-4 mt-4">
+        <button className="bg-red-500 text-white px-4 py-2 rounded-md font-medium mb-8 transition-opacity duration-300 hover:opacity-75">Entre em contato</button>
+        <button className="bg-gray-50  text-red-500 px-4 py-2 rounded-md font-medium mb-8 border-2 border-red-500 transition-opacity duration-300 hover:opacity-75">
+          Saiba mais
+        </button>
+      </div>
+
+      <TextCard
+        title="COMO AJUDAMOS NOSSOS CLIENTES"
+        subtitle="E VAMOS AJUDAR VOCÊ"
+      />
+
+      <Image
+          src={"/homeicons/retangle.svg"}
+          alt="Burger"
+          height={300}
+          width={300}
+        />
+        
+      <Image
+          src={"/homeicons/retangle.svg"}
+          alt="Burger"
+          height={300}
+          width={300}
+        />
+
+    </div>
+  );
 };
 
 export default HeaderSection;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Image from "next/image";
+
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="flex justify-between p-4 bg-gray-50 px-4 py-2 rounded-md font-medium mb-8 rounded-md shadow-lg">
+      <div className="flex items-center">
+        <Image
+          src={"/homeicons/logo.svg"}
+          alt="Logo"
+          height={30}
+          width={30}
+        />
+      </div>
+
+      <div className="flex items-center">
+        <Image
+          src={"/homeicons/burguer.svg"}
+          alt="Burger"
+          height={30}
+          width={30}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
