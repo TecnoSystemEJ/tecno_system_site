e1e2d08a7cc6a8dd70bce9e3757aee045e8e2ba0
Erivan <erivantheone@gmail.com>
2024-02-02 11:49:27 -0300

    Add slider styles

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -8,31 +8,31 @@ type Props = {
 };
 
 const AboutSection = ({ homeData }: Props) => {
-  //const aboutTitle = homeData.data.about_title;
-  //const about_p1 = homeData.data.about_p1;
-  //const about_p2 = homeData.data.about_p2;
-  //const textCard2Title = homeData.data.text_card_2_title;
-  //const textCard2Subtitle = homeData.data.text_card_2_subtitle;
-  //console.log(aboutSectionText);
+  const aboutTitle = homeData.data.about_title;
+  const about_p1 = homeData.data.about_p1;
+  const about_p2 = homeData.data.about_p2;
+  const textCard2Title = homeData.data.text_card_2_title;
+  const textCard2Subtitle = homeData.data.text_card_2_subtitle;
   return (
     <div className="bg-gray-alt ">
       <h2 className="p-4 text-2xl font-primary text-primary-main my-4">
-        TECNOSYSTEM{/*aboutTitle*/}
+        TECNOSYSTEM{}
       </h2>
       <p className="px-4 font-secondary text-black text-xs font-medium mb-8">
         COM BASE EM SÃO PAULO, E ESCRITÓRIOS NA ESPANHA E PORTUGAL, TEMOS UMA
         ATUAÇÃO INTERNACIONAL. NOSSA PAIXÃO É TRANSFORMAR NEGÓCIOS POR MEIO DA
         COMUNICAÇÃO, APLICANDO ESTRATÉGIAS INOVADORAS E CONTANDO COM UMA EQUIPE
         ALTAMENTE CAPACITADA, COLABORANDO PARA CRIAR EXPERIÊNCIAS ENVOLVENTES
-        QUE IMPULSIONAM O ENGAJAMENTO E A CONVERSÃO.{/*about_p1*/}
+        QUE IMPULSIONAM O ENGAJAMENTO E A CONVERSÃO.{}
       </p>
       <p className="px-4 font-secondary text-black text-xs font-medium">
         COM BASE EM SÃO PAULO, E ESCRITÓRIOS NA ESPANHA E PORTUGAL, TEMOS UMA
         ATUAÇÃO INTERNACIONAL. NOSSA PAIXÃO É TRANSFORMAR NEGÓCIOS POR MEIO DA
         COMUNICAÇÃO, APLICANDO ESTRATÉGIAS INOVADORAS E CONTANDO COM UMA EQUIPE
         ALTAMENTE CAPACITADA, COLABORANDO PARA CRIAR EXPERIÊNCIAS ENVOLVENTES
-        QUE IMPULSIONAM O ENGAJAMENTO E A CONVERSÃO.{/*about_p2*/}
+        QUE IMPULSIONAM O ENGAJAMENTO E A CONVERSÃO.{}
       </p>
+
       <TextCard
         title="PROJETOS QUE TEMOS ORGULHO!"
         subtitle="E VAMOS AJUDAR VOCÊ"
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import Image from "next/image";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -7,23 +8,23 @@ import "slick-carousel/slick/slick-theme.css";
 const cards = [
   {
     image: "",
-    title: "back to what I was saying",
-    text: "eu tô de buenas, pegando as muchachas, tequila e limão",
+    title: "Medlembre",
+    text: "the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publish",
   },
   {
     image: "",
-    title: "this could be us",
-    text: "eu tô de buenas, brilhando bastante",
+    title: "Medlembre",
+    text: "the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publish",
   },
   {
     image: "",
-    title: "but you playin'",
-    text: "mais que diamante, e curtindo esse som",
+    title: "Medlembre",
+    text: "the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publish",
   },
 ];
 
 const settings = {
-  dots: false,
+  centerMode: true,
   infinite: false,
   speed: 500,
   slidesToShow: 1,
@@ -32,21 +33,28 @@ const settings = {
 
 const Card = () => {
   return (
-    <div className="m-0 h-72 flex flex-col justify-center bg-gray-alt">
+    <div className="p-4 m-0 h-92 flex flex-col justify-center bg-gray-alt">
       <Slider {...settings}>
         {cards.map((card, index) => {
           return (
             <div
               key={index}
-              className="w-56 h-56 mx-4 p-6 my-20 bg-gray-alt rounded-2xl shadow-xl"
+              className="max-w-80 w-80 h-80 mx-4 p-4 mb-20 bg-gray-alt rounded-2xl shadow-xl"
             >
-              <div className="rounded-2xl">
-                {/*<Image src={} alt="image" height={16} width={44} />*/}
+              <div className="h-28 mb-2 bg-white rounded-xl overflow-hidden">
+                {
+                  <Image
+                    src={`/images/${card.image}`}
+                    alt="image"
+                    height={300}
+                    width={300}
+                  />
+                }
               </div>
-              <h3 className="text-black text-bold font-primary">
+              <h3 className="text-black text-bold font-primary text-lg mb-2">
                 {card.title}
               </h3>
-              <p className="text-black font-secondary">{card.text}</p>
+              <p className="text-black font-secondary text-sm ">{card.text}</p>
             </div>
           );
         })}
