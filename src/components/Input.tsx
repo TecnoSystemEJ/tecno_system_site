8d18d37131ddd3eb2b2e7caeb410eba33819ffed
Luigi <luigimee15@gmail.com>
2024-02-28 20:07:49 -0300

    feat: Conection emailjs with ContactForm component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,16 +4,18 @@ interface Props {
   type: "text" | "email" | "phone";
   placeholder: string;
   value: string;
+  name: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input = ({ type, placeholder, value, onChange }: Props) => {
+const Input = ({ type, placeholder, value, onChange, name }: Props) => {
   return (
     <input
       type={type}
       className="border border-primary-main px-4 py-2 h-14 rounded-xl drop-shadow-sm w-full"
       placeholder={placeholder}
       value={value}
+      name={name}
       onChange={onChange}
     />
   );
